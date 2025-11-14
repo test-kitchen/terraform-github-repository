0263533fbc7c85d941cfc77ff91a50b419f45602
Corey Hemminger <hemminger@hotmail.com>
2025-11-14 11:54:52 -0600

    remove experiments from TF
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- provider.tf
+++ provider.tf
@@ -1,7 +1,4 @@
 terraform {
-  // Enabled so we can have optionals in our objects,
-  // see: https://github.com/hashicorp/terraform/issues/19898
-  experiments = [module_variable_optional_attrs]
   required_providers {
     github = {
       source  = "integrations/github"
